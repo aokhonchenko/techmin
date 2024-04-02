ec5a21853b2c4da706a5357bcd0bfadb51a461f0
aokhonchenko <aleksand.okhonchenko@x5.ru>
2024-04-02 11:26:02 +0300

    hash code

--- src/main/java/ru/x5/techmin/less3/Driver.java
+++ src/main/java/ru/x5/techmin/less3/Driver.java
@@ -5,37 +5,37 @@ import java.util.UUID;
 
 public class Driver {
 
-    private UUID id;
-    private String name;
+  private UUID id;
+  private String name;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
 
-        Driver driver = (Driver) o;
+    Driver driver = (Driver) o;
 
-        return Objects.equals(name, driver.name);
-    }
+    return Objects.equals(name, driver.name);
+  }
 
-    @Override
-    public int hashCode() {
-        return name != null ? name.hashCode() : 0;
-    }
+  @Override
+  public int hashCode() {
+    return name != null ? name.hashCode() : 0;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public UUID getId() {
-        return id;
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+  public void setId(UUID id) {
+    this.id = id;
+  }
 }
